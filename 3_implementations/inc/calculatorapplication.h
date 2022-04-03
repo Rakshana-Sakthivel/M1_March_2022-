367820a600c0de0a900e65a56dcdbaabad2c4fba
Rakshana-Sakthivel <101009876+Rakshana-Sakthivel@users.noreply.github.com>
2022-04-03 07:07:14 +0530

    Add files via upload

---
+++ 3_implementations/inc/calculatorapplication.h
@@ -0,0 +1,29 @@
+#ifndef __CALCULATORAPPLICATION__H_
+#define __CALCULATORAPPLICATION__H_
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+/**
+ * @brief depends on user enter
+ * @param[in] ADDITION
+ * @param[in] SUBTRACTION
+ * @param[in] MULTIPLICATION
+ * @param[in] DIVISION
+ * @param[in] MODULUS
+ * @param[in] FACTORIAL
+ * @param[in] POWER
+ * @param[in] CALCULATOR OPERATIONS
+ * @param[in] C program 
+ * @param[in] c program practical
+ * @return show 
+ * 
+ */
+void calculator_operations();
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+void show();
+#endif
