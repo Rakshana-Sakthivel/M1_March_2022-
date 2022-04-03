5b2d2fe8a97e6dd23efc6826399607c883695dda
Rakshana-Sakthivel <101009876+Rakshana-Sakthivel@users.noreply.github.com>
2022-04-03 07:14:39 +0530

    Add files via upload

---
+++ 3_implementations/src/calculatormain.c
@@ -0,0 +1,15 @@
+#include"calculator.h"
+int main(){
+	input();
+	calculator_operations();
+	 addition();
+     subtraction();
+     multiplication();
+     division();
+     modulus(); 
+     power();
+	resultfunction();
+	show();
+	return 0;
+	
+}
